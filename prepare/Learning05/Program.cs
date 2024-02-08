cea0a59f1053182064ae125620dfaa200ab3c6a5
PeriwinkleMoon <sunrisesunsetforever@gmail.com>
2024-02-08 16:11:25 -0700

    W05 Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("blue", 4);
+
+        // Console.WriteLine(square.GetColor());
+        // Console.WriteLine(square.GetArea());
+
+        Rectangle rectangle = new Rectangle("yellow", 4, 6);
+
+        // Console.WriteLine(rectangle.GetColor());
+        // Console.WriteLine(rectangle.GetArea());
+
+        Circle circle = new Circle("purple", 12);
+
+        // Console.WriteLine(circle.GetColor());
+        // Console.WriteLine(circle.GetArea());
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+            Console.WriteLine("pass");
+        }
     }
 }
